32e8fbf7de3294ef67b513be41fe1bbd0bf889ec
Irakli Keshelashvili <i.keshelashvili@gsi.de>
2026-07-09 13:40:00 +0200

    scifi script

--- scripts/scifi_temp_reader.py
+++ scripts/scifi_temp_reader.py
@@ -29,6 +29,14 @@ def read_fpga_temp(sfp, board):
     bin_str = f"{int(out, 16):016b}"
     val_int = int(bin_str[-16:], 2)
     t_deg = round(val_int * 503.975 / 4096 - 273.15, 1)
+
+    out = Popen(["gosipcmd", "-r", "-x", sfp, dev, "0x20005c"], stdout=PIPE).communicate()[0][2:-2]
+    print(out)
+    bin_str = "{0:016b}".format(int(out, 16))
+    val_int = int(bin_str[-16:], 2)
+    t_deg = round(val_int*503.975/4096-273.15,1)
+    print("SciFi_652 FPGA: {0}".format(t_deg))
+
     return t_deg
 
 
