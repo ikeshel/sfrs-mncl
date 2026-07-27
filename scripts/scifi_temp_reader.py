e331da107d35d65df0666eb311ece34ac8b2cdea
Irakli Keshelashvili <i.keshelashvili@gsi.de>
2026-07-27 21:43:42 +0200

    work in progress from home";git push

--- scripts/scifi_temp_reader.py
+++ scripts/scifi_temp_reader.py
@@ -75,102 +75,37 @@ def main():
         (3, 0), (3, 1),
     ]
 
-    SSH_COMMAND=''
-    for sfp, board in list_sft_board:
-        SSH_COMMAND+=f"/mbs/driv/mbspexV3_5.10-64_DEB/bin/gosipcmd -rxd {sfp} {board} 0x20005c;" # FPGA temperature
-        SSH_COMMAND+=f"/mbs/driv/mbspexV3_5.10-64_DEB/bin/gosipcmd -rxd {sfp} {board} 0x200064;" # SiPM temperature
-    
-    result = subprocess.run(
-        [SSH_COMMAND],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        text=True
-    )
-# SFP: 0x0 Module: 0x0 Address: 0x20005c  Data: 0x105009f9 
-# SFP: 0x0 Module: 0x0 Address: 0x200064  Data: 0x115c 
-# SFP: 0x0 Module: 0x1 Address: 0x20005c  Data: 0x107009f7 
-# SFP: 0x0 Module: 0x1 Address: 0x200064  Data: 0x1171 
-# SFP: 0x0 Module: 0x2 Address: 0x20005c  Data: 0x105009f6 
-# SFP: 0x0 Module: 0x2 Address: 0x200064  Data: 0x1174 
-# SFP: 0x0 Module: 0x3 Address: 0x20005c  Data: 0x105009db 
-# SFP: 0x0 Module: 0x3 Address: 0x200064  Data: 0x1165 
-# SFP: 0x0 Module: 0x4 Address: 0x20005c  Data: 0x105009e9 
-# SFP: 0x0 Module: 0x4 Address: 0x200064  Data: 0x115a 
-# SFP: 0x0 Module: 0x5 Address: 0x20005c  Data: 0x105009e7 
-# SFP: 0x0 Module: 0x5 Address: 0x200064  Data: 0x114d 
-# SFP: 0x1 Module: 0x0 Address: 0x20005c  Data: 0x10700a11 
-# SFP: 0x1 Module: 0x0 Address: 0x200064  Data: 0x11d5 
-# SFP: 0x1 Module: 0x1 Address: 0x20005c  Data: 0x10500a22 
-# SFP: 0x1 Module: 0x1 Address: 0x200064  Data: 0x11a7 
-# SFP: 0x2 Module: 0x0 Address: 0x20005c  Data: 0x105009ff 
-# SFP: 0x2 Module: 0x0 Address: 0x200064  Data: 0x11ab 
-# SFP: 0x2 Module: 0x1 Address: 0x20005c  Data: 0x105009f5 
-# SFP: 0x2 Module: 0x1 Address: 0x200064  Data: 0x11be 
-# SFP: 0x2 Module: 0x2 Address: 0x20005c  Data: 0x105009e9 
-# SFP: 0x2 Module: 0x2 Address: 0x200064  Data: 0x11b8 
-# SFP: 0x2 Module: 0x3 Address: 0x20005c  Data: 0x105009ed 
-# SFP: 0x2 Module: 0x3 Address: 0x200064  Data: 0x11a9 
-# SFP: 0x2 Module: 0x4 Address: 0x20005c  Data: 0x105009f3 
-# SFP: 0x2 Module: 0x4 Address: 0x200064  Data: 0x11ae 
-# SFP: 0x2 Module: 0x5 Address: 0x20005c  Data: 0x105009f3 
-# SFP: 0x2 Module: 0x5 Address: 0x200064  Data: 0x11a0 
-# SFP: 0x3 Module: 0x0 Address: 0x20005c  Data: 0x10500a1f 
-# SFP: 0x3 Module: 0x0 Address: 0x200064  Data: 0x1163 
-# SFP: 0x3 Module: 0x1 Address: 0x20005c  Data: 0x10500a37 
-# SFP: 0x3 Module: 0x1 Address: 0x200064  Data: 0x118e 
-
-    temp_dictionary = {"SFP": 0, "Module": 0, "FPGA": 0.0, "SiPM": 0.0}
+    temp_dictionary = {"SFP": 0, "DEV": 0, "FPGA": 0.0, "SiPM": 0.0}
     temp_list = [
-        {'SFP': 0, 'Module': 0, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 0, 'Module': 1, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 0, 'Module': 2, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 0, 'Module': 3, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 0, 'Module': 4, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 0, 'Module': 5, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 1, 'Module': 0, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 1, 'Module': 1, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 2, 'Module': 0, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 2, 'Module': 1, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 2, 'Module': 2, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 2, 'Module': 3, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 2, 'Module': 4, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 2, 'Module': 5, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 3, 'Module': 0, 'FPGA': 0.0, 'SiPM': 0.0},
-        {'SFP': 3, 'Module': 1, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 0, 'DEV': 0, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 0, 'DEV': 1, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 0, 'DEV': 2, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 0, 'DEV': 3, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 0, 'DEV': 4, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 0, 'DEV': 5, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 1, 'DEV': 0, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 1, 'DEV': 1, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 2, 'DEV': 0, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 2, 'DEV': 1, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 2, 'DEV': 2, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 2, 'DEV': 3, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 2, 'DEV': 4, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 2, 'DEV': 5, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 3, 'DEV': 0, 'FPGA': 0.0, 'SiPM': 0.0},
+        {'SFP': 3, 'DEV': 1, 'FPGA': 0.0, 'SiPM': 0.0},
     ]
 
-    # Parse output line by line
-    for line in result.stdout.strip().split('\n'):
-        temp_dictionary = {"SFP": 0, "Module": 0, "FPGA": 0.0, "SiPM": 0.0}  # Reset dictionary for each line
-        if line.strip():
-            # Extract SFP, Module, Address, and Data
-            parts = line.split()
-            print(f"parts={parts}")
-            
-            sfp   = int(parts[1], 16)
-            board = int(parts[3], 16)
-            
-            # Determine if it's FPGA or SiPM temperature
-            if parts[5] == "0x20005c":
-                temp_dictionary["FPGA"] = parts[7]
-            elif parts[5] == "0x200064":
-                temp_dictionary["SiPM"] = parts[7]
-            
-            # print(f"SFP: {temp_dictionary['SFP']} Module: {temp_dictionary['Module']} {temp_dictionary['FPGA']} {temp_dictionary['SiPM']}")
-            temp_list.append(temp_dictionary.copy())  # Append a copy of the dictionary to the list
+    SSH_COMMAND=''
+    for sfp, board in list_sft_board:
+        temp_fpga = read_fpga_temp(sfp, board)
+        temp_sipm = read_sipm_temp(sfp, board)
+        temp_dictionary = {"SFP": sfp, "DEV": board, "FPGA": temp_fpga, "SiPM": temp_sipm}
+        temp_list.append(temp_dictionary.copy())
+        temp_dictionary.clear()  # Clear the dictionary for the next iteration
+
 
     for temp in temp_list:
         print(temp)
-
-    if result.stderr:
-        print(result.stderr)
-
-
-
-
-        # fpga_temp = read_fpga_temp(sfp, board)
-        # sipm_temp = read_sipm_temp(sfp, board)
-        # print(f" --sfp {sfp} --board {board} --fpga_temp {fpga_temp} --sipm_temp {sipm_temp}")
 
 
 if __name__ == "__main__":
