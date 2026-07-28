3bf03604f68385cc34a1f9d0b3d3608644dff805
Irakli Keshelashvili <i.keshelashvili@gsi.de>
2026-07-28 16:34:43 +0200

    graphs are shown for each board

--- scifi.py
+++ scifi.py
@@ -13,7 +13,7 @@ from loguru import logger
 from Xlib.display import Display
 
 ##
-from PyQt6.QtCore import (Qt, QThread, QThreadPool, QRunnable,  
+from PyQt6.QtCore import (QUrl, Qt, QThread, QThreadPool, QRunnable,  
                           pyqtSlot, pyqtSignal, 
                           QObject, QSignalBlocker, QRect)
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QSizePolicy,
@@ -32,6 +32,7 @@ from mncl_logger      import MnclLogger
 from win_pos_manager  import WindowPositionManager
 from menu_bar         import MenuBarManager
 from ssh_commander    import SSHCommander
+from mbs_browser      import MBSBrowser
 
 # # GUI
 # sys.path.append('gui')
@@ -44,6 +45,29 @@ BOARDS_RIGHT = 2
 BOARDS_LEFT = 2
 BOARDS_TOTAL = BOARDS_UP + BOARDS_DOWN + BOARDS_RIGHT + BOARDS_LEFT
 CHANNEL_TOTAL = BOARD_CHANNEL_NUMBER * BOARDS_TOTAL
+
+SCIFI_BOARD_MAPPING = {
+    0: {'sfp': 0, 'dev': 0},  # Board 1
+    1: {'sfp': 0, 'dev': 1},  # Board 2
+    2: {'sfp': 0, 'dev': 2},  # Board 3
+    3: {'sfp': 0, 'dev': 3},  # Board 4
+    4: {'sfp': 0, 'dev': 4},  # Board 5
+    5: {'sfp': 0, 'dev': 5},  # Board 6
+
+    6: {'sfp': 1, 'dev': 0},  # Board 7
+    7: {'sfp': 1, 'dev': 1},  # Board 8
+
+    8: {'sfp': 2, 'dev': 0},  # Board 9
+    9: {'sfp': 2, 'dev': 1},  # Board 10
+    10: {'sfp': 2, 'dev': 2}, # Board 11
+    11: {'sfp': 2, 'dev': 3}, # Board 12
+    12: {'sfp': 2, 'dev': 4},  # Board 13
+    13: {'sfp': 2, 'dev': 5},  # Board 14
+
+    14: {'sfp': 3, 'dev': 0},  # Board 15
+    15: {'sfp': 3, 'dev': 1},  # Board 16
+    # Add more boards as needed
+}
 
 ###############################################################################
 class SciFiWorkerSignals(QObject):
@@ -98,8 +122,10 @@ class SciFiWorker(QRunnable):
 #==============================================================================
 class Ui_SciFiChannel(object):
 
-    def __init__(self, channel: int):
+    def __init__(self, channel: int, sfp: int = 0, dev: int = 0):
         self.channel = channel
+        self.sfp = sfp
+        self.dev = dev
 
     def setupUi(self, SciFiChannel):
         SciFiChannel.setObjectName(f"SciFiChannel_{self.channel}")
@@ -130,8 +156,10 @@ class Ui_SciFiChannel(object):
 #==============================================================================
 class Ui_BoardInfo(object):
 
-    def __init__(self, board: int):
+    def __init__(self, board: int, sfp: int = 0, dev: int = 0):
         self.board = board
+        self.sfp = sfp
+        self.dev = dev
 
     def setupUi(self, BoardInfo):
         BoardInfo.setObjectName(f"BoardInfo_{self.board}")
@@ -154,22 +182,28 @@ class Ui_BoardInfo(object):
         # SciFi_652 FPGA: 42.2
         # SciFi_652 SiPM sensor: 37.9
         # SciFi FEB sensor: 0.0
-        fpga_temp = 43.2
-        SiPM_temp = 38.9
+        fpga_temp = 0.0
+        SiPM_temp = 0.0
         font = QFont("Arial", 12, QFont.Weight.Bold)
-        lable_alignment = Qt.AlignmentFlag.AlignCenter
+        lable_alignment = Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignVCenter
+
+        self.lab_sfp_dev = QLabel(parent=self.groupBox)
+        self.lab_sfp_dev.setText(f"SFP {self.sfp}, DEV {self.dev}")
+        self.lab_sfp_dev.setFont(QFont("Courier", 12, QFont.Weight.Normal))
+        self.lab_sfp_dev.setAlignment(lable_alignment)
+        self.groupBoxLayout.addWidget(self.lab_sfp_dev, 0, 0)
 
         self.lab_fpga = QLabel(parent=self.groupBox)
         self.lab_fpga.setText(f"FPGA {fpga_temp:.1f} °C")
         self.lab_fpga.setFont(font)
         self.lab_fpga.setAlignment(lable_alignment)
-        self.groupBoxLayout.addWidget(self.lab_fpga, 0, 0) # row, column, rowspan, colspan
+        self.groupBoxLayout.addWidget(self.lab_fpga, 1, 0) # row, column, rowspan, colspan
 
         self.lab_sipm = QLabel(parent=self.groupBox)
         self.lab_sipm.setText(f"SiPM {SiPM_temp:.1f} °C")
         self.lab_sipm.setFont(font)
         self.lab_sipm.setAlignment(lable_alignment)
-        self.groupBoxLayout.addWidget(self.lab_sipm, 1, 0) # row, column, rowspan, colspan
+        self.groupBoxLayout.addWidget(self.lab_sipm, 2, 0) # row, column, rowspan, colspan
 
 
 
@@ -231,15 +265,22 @@ class SciFiMainWindow(  QMainWindow,
         
         self.tab_main_layout.addWidget(SciFi_svg, 1, 1, 2, 6) # add to grid layout
 
+        self.browser_window = MBSBrowser(url=f"http://dtlpc019.gsi.de:17665/retrieval/ui/viewer/archViewer.html?pv=SFRS:FHF1:SCIFI3:SFP0:DEV2:FPGA:TEMP")
+
         self.board = [None]*BOARDS_TOTAL # 
         # self.board = [None]*16 # 
+
         layout = [(1,0), (2,0), (3,0), (4,0), (5,0), (6,0), 
                   (0,1), (0,2), 
                   (1,3), (2,3), (3,3), (4,3), (5,3), (6,3), 
                   (7,1), (7,2)]
         for i in range(len(self.board)):
-            self.board[i] = Ui_BoardInfo(i)
+            sfp=SCIFI_BOARD_MAPPING[i]['sfp']
+            dev=SCIFI_BOARD_MAPPING[i]['dev']
+            self.board[i] = Ui_BoardInfo(i, sfp, dev)
             self.board[i].setupUi(self)
+            self.board[i].lab_fpga.mousePressEvent = lambda event, sfp=sfp, dev=dev: self.show_hide_dashboard("FPGA", sfp, dev) # connect the mouse click event to the show_hide_dashboard function
+            self.board[i].lab_sipm.mousePressEvent = lambda event, sfp=sfp, dev=dev: self.show_hide_dashboard("SIPM", sfp, dev) # connect the mouse click event to the show_hide_dashboard function
             self.tab_main_layout.addWidget(self.board[i].layoutWidget, layout[i][1], layout[i][0]) # add to grid layout
 
         self.tab_main.setLayout(self.tab_main_layout)
@@ -302,6 +343,21 @@ class SciFiMainWindow(  QMainWindow,
         self.setWindowTitle("SciFi Manager")
         self.show()
         self.raise_()
+
+    #==========================================================================
+    def show_hide_dashboard(self, sensor="FPGA", sfp=0, dev=0):
+
+        url=f"http://dtlpc019.gsi.de:17665/retrieval/ui/viewer/archViewer.html?pv=SFRS:FHF1:SCIFI3:SFP{sfp}:DEV{dev}:{sensor}:TEMP"
+        self.browser_window.view.load(QUrl(url))
+
+        if self.browser_window.isVisible():
+            self.browser_window.close() # close the existing window before opening a new one
+        else:
+            self.browser_window.move(200, 20) # move the window to a specific position on the screen
+            self.browser_window.resize(900, 600) # resize the window to a specific size
+            self.browser_window.show()
+            self.browser_window.raise_()
+            self.browser_window.activateWindow()
 
     #==========================================================================
     def on_ckbx_ch_state_changed(self, ch: int, state: int):
